2d4d7a97753a1ad7eb41ad3cf055da0c7f492b44
Lawrence Wong <mystrie@gmail.com>
2025-04-15 22:51:38 +0800

    revert to gpt4o

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def chat(transcript_text):
 
        
     response = client.chat.completions.create(
-        model="gpt-4.1",
+        model="gpt-4o",
         messages=messages,
         temperature=.1,
         # top_p=1,
